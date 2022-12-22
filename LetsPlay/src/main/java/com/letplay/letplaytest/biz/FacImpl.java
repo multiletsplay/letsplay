2e29b99e06bdd775f269741207ab48d9b5aab27a
goyuna <ggyn210@gmail.com>
2022-12-22 15:17:03 +0900

    mvc 패턴

---
+++ LetsPlay/src/main/java/com/letplay/letplaytest/biz/FacImpl.java
@@ -0,0 +1,8 @@
+package com.letplay.letplaytest.biz;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FacImpl implements FacBiz{
+
+}
---
+++ LetsPlay/src/main/java/com/letplay/letplaytest/dao/LessonMapper.java
@@ -0,0 +1,8 @@
+package com.letplay.letplaytest.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface LessonMapper {
+
+}
