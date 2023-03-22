33ee3c8ba5335b5e2ec31b5bf3e265db4cf2b61e
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-03-22 16:25:26 +0900

    gnb 추가 및 마이페이지 css 수

--- LetsPlay/src/main/webapp/js/home.js
+++ LetsPlay/src/main/webapp/js/home.js
@@ -1,4 +1,16 @@
 // ------------
-// MENU GNB
+// 디테일 캐러셀
 // ------------
 
+
+//모든 div의 참조값이 순서대로 담겨있는 배열 얻어내기
+const items = document.querySelectorAll("li.nav__item a")
+
+
+// 위의 모든 div에 "mouseover" 이벤트 리스너 함수를 등록해 보세요.
+for(let i=0;i<items.length;i++){
+  items[i].addEventListener("click",function(){
+    items[i].style.color = 'red';
+      console.log('click')
+    });
+}
