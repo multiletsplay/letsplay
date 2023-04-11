5561a60ec05e9723c29c9be724afe6ab19b9292c
goyuna <ggyn210@gmail.com>
2023-04-12 00:00:10 +0900

    aws s3 연결

--- LetsPlay/src/main/java/com/letplay/letplaytest/biz/FacImpl.java
+++ LetsPlay/src/main/java/com/letplay/letplaytest/biz/FacImpl.java
@@ -1,13 +1,19 @@
 package com.letplay.letplaytest.biz;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.letplay.letplaytest.dao.FacMapper;
 import com.letplay.letplaytest.dto.FacDto;
 import com.letplay.letplaytest.dto.FacResDto;
@@ -17,8 +23,13 @@ import com.letplay.letplaytest.dto.TimeDto;
 @Service
 public class FacImpl implements FacBiz{
 	
+	@Value("{cloud.aws.s3.bucket}")
+	private String bucket ; //Bucket 이름
+	
 	@Autowired
 	private FacMapper facMapper;
+	@Autowired
+	AmazonS3Client amazonS3Client;
 
 	@Override
 	public List<FacDto> selectFacList(String id) {
@@ -47,13 +58,24 @@ public class FacImpl implements FacBiz{
 	
 	@Override
 	public int insertFac(FacDto dto, MultipartFile file) throws Exception {
-		String projpath = System.getProperty("user.dir")+"/src/main/webapp/image";
+		//String projpath = System.getProperty("user.dir")+"/src/main/webapp/image";
 		UUID uuid = UUID.randomUUID();
 		String filename = uuid+"_"+file.getOriginalFilename();
-		File saveFile = new File(projpath,filename);
-		file.transferTo(saveFile);
+		//File saveFile = new File(projpath,filename);
+		
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setContentType(file.getContentType());
+		
+		try(InputStream inputStream = file.getInputStream()){
+			amazonS3Client.putObject(new PutObjectRequest(bucket, filename, inputStream, metadata)
+					.withCannedAcl(CannedAccessControlList.PublicRead));
+		} catch(IOException e) {
+			System.out.println("IOExcetpion");
+		}
+
+//		file.transferTo(saveFile);
 		dto.setFacImg(filename);
-		dto.setFacImgpath("/image/"+filename);
+		dto.setFacImgpath(amazonS3Client.getUrl(bucket, filename).toString());
 		return facMapper.insertFac(dto);
 	}
 	
